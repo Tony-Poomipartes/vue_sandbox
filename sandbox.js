d5096809ed1f0c0624e14524baced538372a3b58
Tony-Poomipartes <tony.p.pro@gmail.com>
2024-03-10 18:34:45 +0100

    � test:   improv algorythms

---
+++ sandbox.js
@@ -0,0 +1,31 @@
+// write a function that adds a list of numbers
+function add(...nums) {
+  let sum = 0
+  for (let num of nums){
+    sum += num
+  }
+  return sum
+}
+
+
+
+console.log("test 3");
+console.log("test 1", add(1, 2, 3, 4, 5));
+console.log("test 2", add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+//5 Write a function that takes an array of numbers as input and returns the sum of all the positive numbers in the array.
+//5 Once you've written your solution, you can test it with different arrays to see if it returns the correct sum. If you'd like, you can share your solution with me and I can provide feedback.
+
+let addArr =(arr) =>
+{
+  let sum = 0
+  for (const numbers of arr) {
+    if (numbers > 0) {
+      sum += numbers
+    }
+  }
+}
+
+console.log("test 3");
+console.log("test 1", addArr([1, 2, 3, 4, 5]));
+console.log("test 2", addArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
